8f06bb3f5d7355913f6a0a53c88467b6a893a922
Nopeka <nxpee3@gmail.com>
2023-04-21 18:47:54 +0300

    add Currencies model

---
+++ src/main/java/com/nxpee/currency_exchanger/model/Currencies.java
@@ -0,0 +1,50 @@
+package com.nxpee.currency_exchanger.model;
+
+public class Currencies {
+    private Integer id;
+    private String code;
+    private String fullName;
+    private String sign;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getSign() {
+        return sign;
+    }
+
+    public void setSign(String sign) {
+        this.sign = sign;
+    }
+
+    public Currencies() {
+    }
+
+    public Currencies(Integer id, String code, String fullName, String sign) {
+        this.id = id;
+        this.code = code;
+        this.fullName = fullName;
+        this.sign = sign;
+    }
+}
