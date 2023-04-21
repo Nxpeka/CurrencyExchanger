30694378673cb24ef8203bf1cedee7fad253c9e6
Nopeka <nxpee3@gmail.com>
2023-04-21 18:10:34 +0300

    add properties and scripts for database

---
+++ src/main/resources/jdbc/insert/insert-currencies.sql
@@ -0,0 +1,5 @@
+INSERT INTO currencies (code, full_name, sign) VALUES ('EUR', 'Euro', 978),
+                                                      ('UAH', 'Hryvnia', 980),
+                                                      ('USD', 'US Dollar', 840),
+                                                      ('BYN', 'Belarussian Ruble', 933),
+                                                      ('RUB', 'Russian Ruble', 643);
---
+++ src/main/resources/jdbc/insert/insert-exchange_rates.sql
@@ -0,0 +1,2 @@
+INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate) VALUES (5, 3, 0.012),
+                                                                               (3, 5, 81.54);
