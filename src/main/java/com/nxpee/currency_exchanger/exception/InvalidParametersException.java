63dbb5c991fb8ed219977921e803b35515ed376e
Nopeka <nxpee3@gmail.com>
2023-04-29 20:38:12 +0300

    add: InvalidParametersException

---
+++ src/main/java/com/nxpee/currency_exchanger/exception/InvalidParametersException.java
@@ -0,0 +1,7 @@
+package com.nxpee.currency_exchanger.exception;
+
+public class InvalidParametersException extends Exception{
+    public InvalidParametersException(String errorMessage){
+        super(errorMessage);
+    }
+}
