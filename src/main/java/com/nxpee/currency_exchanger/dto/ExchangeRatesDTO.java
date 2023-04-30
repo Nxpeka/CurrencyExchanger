027f38516930582e25e9808356d2dc564df2bbad
Nopeka <nxpee3@gmail.com>
2023-04-30 14:21:28 +0300

    refactor: currencyId to currency

--- src/main/java/com/nxpee/currency_exchanger/dto/ExchangeRatesDTO.java
+++ src/main/java/com/nxpee/currency_exchanger/dto/ExchangeRatesDTO.java
@@ -2,23 +2,23 @@ package com.nxpee.currency_exchanger.dto;
 
 public class ExchangeRatesDTO {
     private final Integer id;
-    private final Integer baseCurrencyId;
-    private final Integer targetCurrencyId;
+    private final CurrenciesDTO baseCurrency;
+    private final CurrenciesDTO targetCurrency;
     private final Double rate;
-    public ExchangeRatesDTO(Integer id, Integer baseCurrencyId, Integer targetCurrencyId, Double rate) {
+    public ExchangeRatesDTO(Integer id, CurrenciesDTO baseCurrencyId, CurrenciesDTO targetCurrencyId, Double rate) {
         this.id = id;
-        this.baseCurrencyId = baseCurrencyId;
-        this.targetCurrencyId = targetCurrencyId;
+        this.baseCurrency = baseCurrencyId;
+        this.targetCurrency = targetCurrencyId;
         this.rate = rate;
     }
     public Integer getId() {
         return id;
     }
-    public Integer getBaseCurrencyId() {
-        return baseCurrencyId;
+    public CurrenciesDTO getBaseCurrency() {
+        return baseCurrency;
     }
-    public Integer getTargetCurrencyId() {
-        return targetCurrencyId;
+    public CurrenciesDTO getTargetCurrency() {
+        return targetCurrency;
     }
     public Double getRate() {
         return rate;
