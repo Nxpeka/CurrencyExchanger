a6fe05ed782626864c7d8c47eba1c7c836dca201
Nopeka <nxpee3@gmail.com>
2023-04-30 19:17:32 +0300

    refactor: remove unused method from EncodingFilter

--- src/main/java/com/nxpee/currency_exchanger/servlets/EncodingFilter.java
+++ src/main/java/com/nxpee/currency_exchanger/servlets/EncodingFilter.java
@@ -9,12 +9,6 @@ import java.nio.charset.StandardCharsets;
 
 @WebFilter(urlPatterns = "/*")
 public class EncodingFilter implements Filter {
-    public void init(FilterConfig config) throws ServletException {
-    }
-
-    public void destroy() {
-    }
-
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
         response.setCharacterEncoding(StandardCharsets.UTF_8.name());
