91bb6dbe377fb30f828095f91ae4856fccefc4fb
Nopeka <nxpee3@gmail.com>
2023-04-30 20:56:19 +0300

    feat: add doPatch in ExchangeRateServlet

--- src/main/java/com/nxpee/currency_exchanger/servlets/exchangeRates/ExchangeRateServlet.java
+++ src/main/java/com/nxpee/currency_exchanger/servlets/exchangeRates/ExchangeRateServlet.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nxpee.currency_exchanger.dto.ExchangeRatesDTO;
 import com.nxpee.currency_exchanger.exception.InvalidParametersException;
 import com.nxpee.currency_exchanger.service.ExchangeRatesService;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -18,6 +19,39 @@ import java.util.Optional;
 public class ExchangeRateServlet extends HttpServlet {
 
     private final ExchangeRatesService exchangeRatesService = ExchangeRatesService.getInstance();
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if (req.getMethod().equalsIgnoreCase("PATCH")){
+            this.doPatch(req, resp);
+        } else {
+            super.service(req, resp);
+        }
+    }
+
+    protected void doPatch(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String requestPair = req.getPathInfo().replace("/", "").toUpperCase();
+        try{
+            ObjectMapper objectMapper = new ObjectMapper();
+            PrintWriter writer = resp.getWriter();
+            Optional<ExchangeRatesDTO> exchangeRatePair = exchangeRatesService.findPair(requestPair);
+            if (exchangeRatePair.isPresent()) {
+                ExchangeRatesDTO exchangeRate = exchangeRatePair.get();
+                String parameter = req.getReader().readLine().toLowerCase();
+
+                ExchangeRatesDTO updatedRate = exchangeRatesService.updateRate(exchangeRate, parameter);
+
+                String rate = objectMapper.writeValueAsString(updatedRate);
+                writer.write(rate);
+            } else {
+                resp.sendError(404, "ExchangeRate not found");
+            }
+
+        } catch (SQLException e) {
+            resp.sendError(500, e.getMessage());
+        } catch (InvalidParametersException e) {
+            resp.sendError(400, e.getMessage());
+        }
+    }
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         String requestPair = req.getPathInfo().replace("/", "").toUpperCase();
