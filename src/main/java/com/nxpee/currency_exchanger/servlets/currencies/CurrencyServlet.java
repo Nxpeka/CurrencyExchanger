b6d61b7cf2309e889cf627340c078082e0a43b6f
Nopeka <nxpee3@gmail.com>
2023-04-30 14:53:31 +0300

    add: CurrencyServlet

---
+++ src/main/java/com/nxpee/currency_exchanger/servlets/currencies/CurrencyServlet.java
@@ -0,0 +1,39 @@
+package com.nxpee.currency_exchanger.servlets.currencies;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nxpee.currency_exchanger.dto.CurrenciesDTO;
+import com.nxpee.currency_exchanger.exception.InvalidParametersException;
+import com.nxpee.currency_exchanger.service.CurrenciesService;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.Optional;
+
+@WebServlet("/currency/*")
+public class CurrencyServlet extends HttpServlet {
+
+    private final CurrenciesService currenciesService = CurrenciesService.getInstance();
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String requestCode = req.getPathInfo().replace("/", "").toUpperCase();
+        try{
+            ObjectMapper objectMapper = new ObjectMapper();
+            PrintWriter writer = resp.getWriter();
+            Optional<CurrenciesDTO> currency = currenciesService.findByCode(requestCode);
+            if (currency.isPresent()){
+                writer.write(objectMapper.writeValueAsString(currency.get()));
+            } else {
+                resp.sendError(404, "Currency not found");
+            }
+        }catch (SQLException e) {
+            resp.sendError(500, e.getMessage());
+        } catch (InvalidParametersException e) {
+            resp.sendError(400, e.getMessage());
+        }
+    }
+}
