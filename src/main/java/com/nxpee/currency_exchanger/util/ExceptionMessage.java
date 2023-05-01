7d9a91978ea64875d37f7d1a96585ec0860c81d1
Nopeka <nxpee3@gmail.com>
2023-05-01 18:13:50 +0300

    add: ExceptionMessage

---
+++ src/main/java/com/nxpee/currency_exchanger/util/ExceptionMessage.java
@@ -0,0 +1,13 @@
+package com.nxpee.currency_exchanger.util;
+
+public class ExceptionMessage {
+    private final String message;
+
+    public ExceptionMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
